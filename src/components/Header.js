cd4dd6e1ffc591756151e166b520d2a70e741617
scattamooch <seanstevens729@gmail.com>
2023-06-14 17:21:14 -0400

    completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+function Header(props) {
+    return (
+        <header>
+        <h2>Shopster</h2>
+        <button onClick={props.onDarkModeClick}>
+            {props.isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+        </header>
+    )
+}
+
+export default Header;
